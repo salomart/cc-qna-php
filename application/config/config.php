8929430bd832fb93b42b8c96fa46f5a74d7e22ac
Salomon Martinez <salomart@outlook.com>
2019-02-12 23:46:44 -0600

    Revert "Revert "Changed base_url for bluemix deployments""
    This reverts commit 974888e9c0990426e5edb714dd6ff24b8dcfa62b.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cc-qna/';
+$config['base_url'] = 'https://cc-qna.mybluemix.net/';
 
 /*
 |--------------------------------------------------------------------------
